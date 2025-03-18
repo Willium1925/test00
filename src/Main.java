2196fec660758c3fadecef6275f5c02971b4fbc4
LEE CHEN WEI <D1397026@o365.fcu.edu.tw>
2025-03-18 13:02:01 +0800

    Update Main.java
    1232131

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!1321321");
+        System.out.println("Hello, World!123121321321321321");
     }
 }
